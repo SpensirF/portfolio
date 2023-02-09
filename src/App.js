230344a50a088251e12c18ce6e97205f0c386a40
spensirF <spensirfields@gmail.com>
2023-02-09 14:15:19 -0800

    fixed Home route

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import Home from './components/Home';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
-        <Route index element={<Home />} />
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
