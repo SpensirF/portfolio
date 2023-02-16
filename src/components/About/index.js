e3eed292b484386b5db9c308d1c2ffec7834cd63
spensirF <spensirfields@gmail.com>
2023-02-15 16:44:54 -0800

    added text and spin cube to about page

---
+++ src/components/About/index.js
@@ -0,0 +1,70 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3, faHtml5, faJsSquare, faPhp, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+
+
+
+const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    useEffect (()=> {
+        setTimeout(() => {
+           setLetterClass('text-animate-hover')
+       }, 3000)
+   }, [])
+
+    return (
+        <>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                    idx={15}
+                    />
+                </h1>
+                <p>
+                    I’m a software development engineer with over 1000 hours of experience in a short amount of time learning multiple languages, frameworks, and databases such as: Python, C#, MERN, Javascript and SQL. After Graduating from Coding Dojo Bootcamp, I couldn't wait to test my experience and interests in creating projects that will have a positive impact on the world. Example of this, is a project I am currently working on that will help with mental health for people that were affected by the pandemic. 
+                </p>
+                <p>
+                    In my free time, you can find me being creative is multiple mediums like: videography, streaming (via Twitch), entertaining, and in music. Outside of creating I am very passionate about video games. Currently I am self-proclaim semi-pro when it comes to fighting games haha, but love taking part in all games.
+                </p>
+                
+            </div>
+
+            <div className="stage-cube-cont">
+                <div className="cubespinner">
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faPython} color="#DD0031" />
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                    </div>
+                    <div className="face4">
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faPhp} color="#EC4D28" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Loader type="pacman" />
+        </>
+    )
+
+}
+
+
+
+export default About 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,48 @@
+import { useEffect, useState } from 'react';
 import LogoTitle from '../../assets/images/logo-s.png';
 import {Link} from 'react-router-dom';
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['p', 'e', 'n', 'c', 'e','r']
+    const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.' ]
+
+    useEffect (()=> {
+         setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm 
+                <h1> 
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _13`}>i</span>
+                <span className={`${letterClass} _14`}>y</span>
+                <span className={`${letterClass} _15`}>a</span>
+                <span className={`${letterClass} _16`}>h!</span>
+                <br/> 
+                <span className={`${letterClass} _17`}>I</span>
+                <span className={`${letterClass} _18`}>'m</span>
                 <img src={LogoTitle} alt="developer" />
-                pencer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
                 <br/>
-                Software developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22} />
                 </h1>
-                <h2>Frontend Developer / Backend Developer / Gamer / Videographer</h2>
+                <h2>Full Stack Developer / Gamer / Videographer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
 
             </div>
-
+        <Logo />
         </div>
     );
 }
