ebe959682c9940c7e61eab10252927b21b16fbe2
spensirF <spensirfields@gmail.com>
2023-02-21 16:20:55 -0800

    finished my work page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '../../assets/images/logo-sf.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faSuitcase, faUser} from '@fortawesome/free-solid-svg-icons'
